d5e89bb24f1fd382fe955333116d60465222b460
Jonathan Ngondi <jonathan.ngondi@andela.com>
2017-05-23 16:01:11 +0300

    Documentation and Imports -Adds new modules to imports -Adds verify_token method to user -Adds docstrings for each class and method

--- app/v1/models.py
+++ app/v1/models.py
@@ -2,12 +2,16 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
-from flask import request, abort
+from flask import request, abort, jsonify
 import jwt
 import datetime
 
 
 class User(db.Model):
+    """Class for creating Bucketlist schema,as well as CRUD methods, as well as static methods 
+       for user validation, as well as user verification.
+    """
+
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -21,30 +25,46 @@ class User(db.Model):
         self.email = email
         self.password = password
         self.created_at = datetime.datetime.now()
-       
     
+
     @staticmethod
     def hash_password(password):
-         return generate_password_hash(password)
-    
+        """This method hashes a user password."""
+        return generate_password_hash(password)
+
     @staticmethod
     def check_password(hash, password):
+        """Checks and verifies the password of a user login."""
         return check_password_hash(hash,password)
     
-    def generate_token(data, secret):
+    def generate_token(self, data, secret):
+        """Generates a token for user session."""
         return jwt.encode(data, secret, exp=86400)
-    
+
+    @staticmethod
+    def verify_token(secret):
+        """Verifies the token passed in the header."""
+        token = request.headers.get("Token")
+        if token is None:
+            abort(401)
+        try:
+            decoded_data = jwt.decode(token, secret)
+        except jwt.ExpiredSignatureError:
+            return jsonify({"message":"Your token has expired, please login again."})
+        except jwt.InvalidTokenError:
+            return jsonify({"message":"Your token is invalid, use a valid token"})
+        return decoded_data
+
     def save(self):
+        """Saves data to database."""
         db.session.add(self)
-        db.session.commit()
-    
-    def update(self):
-        db.session.update(self)
         db.session.commit()
 
 
 class Bucketlist(db.Model):
-    """This class represents the bucketlist table."""
+    """Class for creating Bucketlist schema,as well as CRUD methods, as well as some query
+       methods.
+    """
 
     __tablename__ = 'bucketlists'
 
@@ -55,27 +75,16 @@ class Bucketlist(db.Model):
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
                               onupdate=db.func.current_timestamp())
     user = db.relationship("User", back_populates="user_bucketlists")
+
     def __init__(self, name, created_by):
         """Initialize tables with name"""
         self.name = name
         self.created_by = created_by
+
     def save(self):
+        """Saves bucketlist data to database"""
         db.session.add(self)
         db.session.commit()
-    
-    @staticmethod
-    def verify_token(secret):
-        token = request.headers.get("Token")
-        if token is None:
-            abort(401)
-        try:
-            decoded_data = jwt.decode(token, secret)
-        except jwt.ExpiredSignatureError:
-            return jsonify({"message":"Your token has expired, please login again."}), 401
-        except jwt.InvalidTokenError:
-            return jsonify({"message":"Your token is invalid, use a valid token"}), 401
-        return decoded_data
-        
 
     @staticmethod
     def get_all():
@@ -84,12 +93,15 @@ class Bucketlist(db.Model):
     def delete(self):
         db.session.delete(self)
         db.session.commit()
+    
+
 
     def __repr__(self):
         return "<Bucketlist: {}".format(self.name)
 
 class Items(db.Model):
-    
+    """Class for creating Items schema in database as well as methods for CRUD operations."""
+
     __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
@@ -97,7 +109,7 @@ class Items(db.Model):
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
                               onupdate=db.func.current_timestamp())
-    
+
     def __init__(self, name, bucketlist_id):
         self.name = name
         self.bucketlist_id = bucketlist_id
@@ -105,7 +117,7 @@ class Items(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
-    
+
     def delete(self):
         db.session.delete(self)
         db.session.commit()
